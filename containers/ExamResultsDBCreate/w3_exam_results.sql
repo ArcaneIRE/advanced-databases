ecc7429e2d82d5738def65c2ac4282179cb49d5f
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2024-10-15 16:17:12 +0100

    Start from scratch

--- containers/ExamResultsDBCreate/w3_exam_results.sql
+++
@@ -1,7 +0,0 @@
-set search_path to "examresultsdb";
-
-SELECT s.StudentID, s.FirstName, s.LastName, er.ExamID, er.examdate, er.Score, er.Grade
-FROM
-    Students s
-    JOIN ExamResults er ON s.StudentID = er.StudentID
-    JOIN Exams e ON er.ExamID = e.ExamID;
